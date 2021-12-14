34faf03889b5733e275b09878bf5993c37c6967a
Oleksandra Baga <oleksandra.baga@gmail.com>
2021-12-14 13:36:34 +0100

    added enemy script

---
+++ BlockBuster/Assets/Scripts/CameraRig.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public GameObject target;
+
+    private Transform rigTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  gets a reference to the parent Camera object's transform in the scene hierarchy. 
+        rigTransform = this.transform.parent;
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        rigTransform.position = Vector3.Lerp(rigTransform.position, target.transform.position,
+          Time.deltaTime * moveSpeed);
+    }
+}
---
+++ BlockBuster/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public int health;
+    public int damage;
+    public Transform targetTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
