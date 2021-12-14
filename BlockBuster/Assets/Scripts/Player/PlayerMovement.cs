6fa142b1a55aceda44ffadb972d27e0f80fdc2af
Oleksandra Baga <oleksandra.baga@gmail.com>
2021-12-14 12:35:08 +0100

    first C# Script

--- BlockBuster/Assets/Scripts/Player/PlayerMovement.cs
+++ BlockBuster/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,19 +7,39 @@ public class NewBehaviourScript : MonoBehaviour
     public float acceleration;
     public float maxSpeed;
 
+    // Adding a Rigidbody component to an object will put its motion under the control of Unity's physics engine.
+    // // Even without adding any code, a Rigidbody object will be pulled downward by gravity and
+    // // will react to collisions with incoming objects if the right Collider component is also present.
     private Rigidbody rigidBody;
+
+    // Enum. Key codes returned by Event.keyCode. These map directly to a physical key on the keyboard.
     private KeyCode[] inputKeys;
     private Vector3[] directionsForKeys;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputKeys = new KeyCode[] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+        directionsForKeys = new Vector3[] { Vector3.forward, Vector3.left, Vector3.back, Vector3.right };
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is frame rate independent and should be used when working with Rigidbodies.
+    // method will be fired at a constant interval
+    void FixedUpdate()
     {
-        
+        // loop checks to see if any of the input keys were pressed.
+        for (int i = 0; i < inputKeys.Length; i++)
+        {
+            var key = inputKeys[i];
+
+            // 2
+            if (Input.GetKey(key))
+            {
+                // 3
+                Vector3 movement = directionsForKeys[i] * acceleration * Time.deltaTime;
+            }
+        }
+
     }
 }
