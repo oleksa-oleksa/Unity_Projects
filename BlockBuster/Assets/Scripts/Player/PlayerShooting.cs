ecb6848fa7b4eb7e7a9abf28dabdd399fbce421d
Oleksandra Baga <oleksandra.baga@gmail.com>
2021-12-14 15:02:12 +0100

    Firing Projectiles

---
+++ BlockBuster/Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    public Projectile projectilePrefab;
+    public LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void shoot(RaycastHit hit)
+    {
+        // 1
+        var projectile = Instantiate(projectilePrefab).GetComponent<Projectile>();
+        // This point always looks like (x, 0.5, z). X and Z are coordinates on the floor
+        // where the ray cast from the mouse click position hits. 
+        var pointAboveFloor = hit.point + new Vector3(0, this.transform.position.y, 0);
+
+        // 3
+        var direction = pointAboveFloor - transform.position;
+
+        // 4
+        var shootRay = new Ray(this.transform.position, direction);
+        Debug.DrawRay(shootRay.origin, shootRay.direction * 100.1f, Color.green, 2);
+
+        // 5
+        Physics.IgnoreCollision(GetComponent<Collider>(), projectile.GetComponent<Collider>());
+
+        // 6
+        projectile.FireProjectile(shootRay);
+    }
+
+    void raycastOnMouseClick()
+    {
+        RaycastHit hit;
+        Ray rayToFloor = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(rayToFloor.origin, rayToFloor.direction * 100.1f, Color.red, 2);
+
+        if (Physics.Raycast(rayToFloor, out hit, 100.0f, mask, QueryTriggerInteraction.Collide))
+        {
+            shoot(hit);
+        }
+    }
+
+    // 2
+    void Update()
+    {
+        bool mouseButtonDown = Input.GetMouseButtonDown(0);
+        if (mouseButtonDown)
+        {
+            raycastOnMouseClick();
+        }
+    }
+}
