8b09e59abe79cd777146509996bde8743c7de5b8
Oleksandra Baga <oleksandra.baga@gmail.com>
2021-12-14 12:15:51 +0100

    first C# Script

---
+++ BlockBuster/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float acceleration;
+    public float maxSpeed;
+
+    private Rigidbody rigidBody;
+    private KeyCode[] inputKeys;
+    private Vector3[] directionsForKeys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
