b4045e7577d1b5c2067aaf3fcde950ff41058a64
Oleksandra Baga <oleksandra.baga@gmail.com>
2021-12-27 21:50:55 +0100

    CSV logger is separated from TextMesh user info box

--- HeadPosition/Assets/Scripts/UserCoordinatesTextMesh.cs
+++ HeadPosition/Assets/Scripts/UserCoordinatesTextMesh.cs
@@ -13,48 +13,20 @@ public class UserCoordinatesTextMesh : MonoBehaviour
     public TextMeshPro textmeshPro;
 
     // Camera 
-    public Vector3 headposition;
-    public Quaternion orientation;
-    public Vector3 orientationEulerAngles;
-    public Vector3 velocity;
-    public double speed;
+    private Vector3 headposition;
+    private Quaternion orientation;
+    private Vector3 orientationEulerAngles;
+    private Vector3 velocity;
+    private double speed;
 
     // time variables for delay
     private float waitTime = 1.0f;
     private float timer = 0.0f;
 
-    // for CSV Logger
-    private string filePath;
-    private StreamWriter outStream;
-
-
-    // Following method is used to retrive the relative path as device platform
-    private string getPath()
-    {
-#if UNITY_EDITOR
-        return Application.dataPath +"/CSV/"+"Saved_data.csv";
-#elif UNITY_ANDROID
-        return Application.persistentDataPath+"Saved_data.csv";
-#elif UNITY_IPHONE
-        return Application.persistentDataPath+"/"+"Saved_data.csv";
-#elif WINDOWS_UWP
-        return Application.persistentDataPath  + "/CSV/";
-#else
-        return Application.dataPath + "/CSV/";
-#endif
-    }
-
 
     void Start()
     {
       
-        OpenCSVFile();
-        WriteHeaderToCSV();
-
-
-
-        // original 
-        //Save();
     }
 
   
@@ -62,38 +34,34 @@ public class UserCoordinatesTextMesh : MonoBehaviour
     {
         
         timer += Time.deltaTime;
-        
-        headposition = Camera.main.transform.position;
-        orientation = Camera.main.transform.rotation;
-        orientationEulerAngles = Camera.main.transform.eulerAngles;
-        velocity = Camera.main.velocity;
-        speed = Math.Sqrt(Math.Pow(velocity[0], 2) + Math.Pow(velocity[1], 2) + Math.Pow(velocity[2], 2));
+                
+        float x = Camera2CSV.headposition.x;
+        float y = Camera2CSV.headposition.y;
+        float z = Camera2CSV.headposition.z;
 
-        // timestamp,x,y,z,qx,qy,qz,qw - structure for CSV
-        // creates values for CSV
-        float timestamp = Time.time;
-        float x = headposition.x;
-        float y = headposition.y;
-        float z = headposition.z;
-        float qx = orientation.x;
-        float qy = orientation.y;
-        float qz = orientation.z;
-        float qw = orientation.w;
+        /*
+        float qx = Camera2CSV.orientation.x;
+        float qy = Camera2CSV.orientation.y;
+        float qz = Camera2CSV.orientation.z;
+        float qw = Camera2CSV.orientation.w;
 
+        float speed = (float)Camera2CSV.speed;
+
+        */
+
+        float eu_x_pitch = Camera2CSV.orientationEulerAngles.x; // transverse axis 
+        float eu_y_yaw = Camera2CSV.orientationEulerAngles.y; // vertical axis
+        float eu_z_roll = Camera2CSV.orientationEulerAngles.z; // longitudial axis
 
         // Check if we have reached the time limit
         if (timer > waitTime)
         {
 
-            // save camera data from current frame in CSV file
-            WriteFrameToCSV(timestamp, x, y, z, qx, qy, qz, qw);
-
             // update text block in hologram 
             textmeshPro = GetComponent<TextMeshPro>();
             textmeshPro.SetText("Position x: {0:3}, y: {1:3}, z: {2:3}; \r\n " +
-                "Velocity x: {3:3}, y: {4:3}, z: {5:3}; \r\n" +
-                "Speed: {6:3}",
-               x, y, z, velocity[0], velocity[1], velocity[2], (float)speed);
+                "Pitch x: {3:3}, Yaw y: {4:3}, Roll z: {5:3}",
+               x, y, z, eu_x_pitch, eu_y_yaw, eu_z_roll);
 
             // reset timer
             timer = 0.0f;
@@ -102,120 +70,4 @@ public class UserCoordinatesTextMesh : MonoBehaviour
     }
 
 
-    void OpenCSVFile() 
-    {
-        //  retriving the relative path as device platform
-       filePath = getPath() + DateTime.Now.ToString("yyyyMMdd_HHmm") + "_saved.csv";
-
-        //  Creates or opens a file for writing UTF-8 encoded text.
-        //  If the file already exists, its contents are overwritten.
-        //  By using a timestamp in file name it is supposed to creare a new csv file
-        //  for every start of HoloLens Application
-        outStream = System.IO.File.CreateText(filePath);
-    }
-
-    void WriteHeaderToCSV()
-    {
-
-        outStream.WriteLine("timestamp,x,y,z,qx,qy,qz,qw");
-        outStream.Flush();
-
-    }
-
-   void WriteFrameToCSV(params float[] cameraData)
-    {
-
-        string delimiter = ", "; // comma with space after 
-        string data = string.Join(delimiter, cameraData);
-
-        outStream.WriteLine(data);
-        outStream.Flush();
-    }
-
-
-    void WriteHeaderToCSV_OLD()
-    {
-
-        //  retriving the relative path as device platform
-        filePath = getPath() + DateTime.Now.ToString("yyyyMMdd_HHmm") + "_saved.csv";
-
-        //  Creates or opens a file for writing UTF-8 encoded text.
-        //  If the file already exists, its contents are overwritten.
-        //  By using a timestamp in file name it is supposed to creare a new csv file
-        //  for every start of HoloLens Application
-        outStream = System.IO.File.CreateText(filePath);
-
-        string header = "timestamp,x,y,z,qx,qy,qz,qw";
-        outStream.WriteLine(header);
-
-        outStream.Flush();
-
-        string tmp = "newnewnew";
-        outStream.WriteLine(tmp);
-
-        outStream.Close();
-
-        /*
-        outStream = System.IO.File.CreateText(filePath);
-        string tmp = "4535435454354";
-        outStream.WriteLine(tmp);
-        */
-    }
-
-    public void OnDestroy()
-    {
-        // OnDestroy occurs when a Scene or game ends.
-        // If a Scene is closed and a new Scene is loaded the OnDestroy call will be made.
-        // When built as a standalone application OnDestroy calls are made when Scenes end.
-
-        // Closes the current StreamWriter object and the underlying stream.
-        outStream.Close();
-
-    }
-
-    /*
-    void Save()
-    {
-
-        // Creating First row of titles manually..
-        string[] rowDataTemp = new string[3];
-        rowDataTemp[0] = "Name";
-        rowDataTemp[1] = "ID";
-        rowDataTemp[2] = "Income";
-        rowData.Add(rowDataTemp);
-
-        // You can add up the values in as many cells as you want.
-        for (int i = 0; i < 10; i++)
-        {
-            rowDataTemp = new string[3];
-            rowDataTemp[0] = "Alexandra" + i; // name
-            rowDataTemp[1] = "" + i; // ID
-            rowDataTemp[2] = "$" + UnityEngine.Random.Range(5000, 10000); // Income
-            rowData.Add(rowDataTemp);
-        }
-
-        string[][] output = new string[rowData.Count][];
-
-        for (int i = 0; i < output.Length; i++)
-        {
-            output[i] = rowData[i];
-        }
-
-        int length = output.GetLength(0);
-        string delimiter = ",";
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int index = 0; index < length; index++)
-            sb.AppendLine(string.Join(delimiter, output[index]));
-
-
-        string filePath = getPath();
-
-        StreamWriter outStream = System.IO.File.CreateText(filePath);
-        outStream.WriteLine(sb);
-        outStream.Close();
-    }
-
-    */
 }
