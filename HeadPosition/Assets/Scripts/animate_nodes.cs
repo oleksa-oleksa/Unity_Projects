3ebff85c8a526503f333f1e7e55caebc28e72687
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-01-09 14:30:12 +0100

    Animation using waitForEndOfFrame()

--- HeadPosition/Assets/Scripts/animate_nodes.cs
+++ HeadPosition/Assets/Scripts/animate_nodes.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class animate_nodes : MonoBehaviour
 {
     public GameObject animation;
-
+    public int targetFramesPerSecond = 120;
+    
     protected GameObject getFrame(int n)
     {
         return animation.transform.GetChild(n).gameObject;
@@ -16,7 +17,45 @@ public class animate_nodes : MonoBehaviour
         return animation.transform.childCount;
     }
 
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        animation = GameObject.Find("Animation");
+
+        for (int i = 0; i < getFrameCount(); i++)
+        {
+            getFrame(i).SetActive(false);    
+        }
+    }
+
+    private IEnumerator Start()
+    {
+        var currentIndex = 0;
+        getFrame(currentIndex).SetActive(true);
+
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            getFrame(currentIndex).SetActive(false);
+            currentIndex = (currentIndex + 1) % getFrameCount();
+            getFrame(currentIndex).SetActive(true);
+        }
+        /*
+         * var currentIndex = 0;
+
+        getFrame(currentIndex).SetActive(true);
+        int frameCount = getFrameCount();
+
+        while (true)
+        {
+            yield return new WaitForSeconds(1f / targetFramesPerSecond);
+            getFrame(currentIndex).SetActive(false);
+            currentIndex = (currentIndex + 1) % frameCount;
+            getFrame(currentIndex).SetActive(true);
+        }
+        */
+    }
+
+/*    // Start is called before the first frame update
     void Start()
     {
         animation = GameObject.Find("Animation");
@@ -25,6 +64,8 @@ public class animate_nodes : MonoBehaviour
         {
             getFrame(i).SetActive(false);
         }
+
+        activeFrame = 0;
     }
 
     // Update is called once per frame
@@ -32,4 +73,5 @@ public class animate_nodes : MonoBehaviour
     {
         
     }
+*/
 }
