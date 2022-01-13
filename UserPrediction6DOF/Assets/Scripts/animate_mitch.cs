54f17a5c88e5f48a55102fe3d48a735acfcc6aa2
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-01-13 16:52:01 +0100

    mitch converted in jpg, mnt corrected, obj object imported into Unity, project deplyed as a separate application

--- UserPrediction6DOF/Assets/Scripts/animate_mitch.cs
+++ UserPrediction6DOF/Assets/Scripts/animate_mitch.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class animate_mitch : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject animationObject_mitch;
+    private float secondsPerFrame = 0.044f;
+
+    private int currentFrame = 0;
+    private float timeAccumulator;
+
+    protected GameObject getFrame(int n)
+    {
+        return animationObject_mitch.transform.GetChild(n).gameObject;
+    }
+
+    protected int getFrameCount()
+    {
+        return animationObject_mitch.transform.childCount;
+    }
+
     void Start()
     {
-        
+        animationObject_mitch = GameObject.Find("Animation_Mitch");
+        timeAccumulator = 0;
+
+        for (int i = 0; i < getFrameCount(); i++)
+        {
+            getFrame(i).SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        timeAccumulator += Time.deltaTime;
+
+        if (timeAccumulator > secondsPerFrame)
+        {
+            getFrame(currentFrame).SetActive(false);
+            currentFrame = (currentFrame + Mathf.RoundToInt(timeAccumulator / secondsPerFrame)) % getFrameCount();
+            getFrame(currentFrame).SetActive(true);
+            timeAccumulator = 0.0f;
+        }
+        /*
+        getFrame(currentFrame).SetActive(false);
+        currentFrame = (currentFrame+1)% getFrameCount();
+        getFrame(currentFrame).SetActive(false);
+        */
     }
+
 }
