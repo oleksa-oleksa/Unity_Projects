ee76bae60508e1c792d0f4145052671d3ae22507
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-01-13 13:39:10 +0100

    started new project, josh slowed

---
+++ UserPrediction6DOF/Assets/Scripts/animate_mitch.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animate_mitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
